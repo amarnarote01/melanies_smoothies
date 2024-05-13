a86a7377592ca2f05b5e9a2c3be1890e187d796d
Amar Narote <113687074+amarnarote01@users.noreply.github.com>
2024-05-13 14:36:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,10 +49,12 @@ if ingredients_list:
             st.success('Your Smoothie is ordered!', icon="✅")
     
     # st.write(my_insert_stmt)
-    st.stop()
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+    st.stop()
+
